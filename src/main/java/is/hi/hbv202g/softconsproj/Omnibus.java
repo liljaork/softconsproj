7c85427ee0e8e3813ef86ca488efec5d62dca995
liljaork <liljaork@gmail.com>
2024-03-26 11:30:53 +0000

    setti upp getter og setter í omnibus

---
+++ src/main/java/is/hi/hbv202g/softconsproj/Omnibus.java
@@ -0,0 +1,32 @@
+package is.hi.hbv202g.softconsproj;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Omnibus {
+    private String nafnaBok;
+
+    public Omnibus(String nafnaBok){
+        this.nafnaBok= nafnaBok;
+
+    }
+
+    public String getNafnaBok(){
+        return nafnaBok;
+    }
+
+    public void setNafnaBok(String nafnaBok){
+        this.nafnaBok= nafnaBok;
+    }
+
+    /* 
+    public List<Book> leigjaBaekur() throws UserOrBookDoesNotExistException{
+
+        while (){
+            findBookByTitle(nafnaBok);
+        }
+        
+    }
+    */
+
+    
+}
