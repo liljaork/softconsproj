7de2a205c6b338b0812053190de025856ce500d4
Silja Ægisdóttir <siljabjork@vr2t-dw481.rhi.hi.is>
2024-04-16 15:27:46 +0000

    Bætti við license svo hægt að runna mvn site nuna og sjá síðuna

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # SoftConsProj
-Verkefni unnið út frá verkefni 8.
+A project developed from project 8, followed by instructions of the "Software Construction Project" in the course hbv202g. 
 
-## Project structure
+## Installation
+This project is a maven-based project so it uses the standard Maven project structure that your IDE should be able to understand when you clone it from git. The provided Maven POM includes the dependency.
 
 ## Design inforfmation
 í þessu verkefni er notast við coposite desgin pattern. Við erum með interface sem kallast LibraryItem og svo tvo klasa sem implementa interface-ið og þeir heita Book og Omnibus
--- src/main/java/is/hi/hbv202g/softconsproj/Book.java
+++ src/main/java/is/hi/hbv202g/softconsproj/Book.java
@@ -9,9 +9,20 @@ public class Book implements LibraryItem {
     private List<Author> authors;
     private boolean cleaning;
 
+    /**
+     * Default constructor. Creates a book with no title or author.
+     */
     public Book() {
     }
     
+    /**
+     * Constructs a book with a specified title and author name. The book
+     * is not in the cleaning process by default.
+     *
+     * @param title       The title of the book.
+     * @param authorName  The name of the author of the book.
+     * @throws IllegalArgumentException if the title is null.
+     */
     public Book(String title, String authorName) {
         if (title == null) {
             throw new IllegalArgumentException("Title cannot be null");
@@ -22,6 +33,13 @@ public class Book implements LibraryItem {
         this.cleaning = false;
     }
 
+    /**
+     * Constructs a book with a specified title and a list of authors.
+     *
+     * @param title   The title of the book.
+     * @param authors A list of Author objects representing the authors of the book.
+     * @throws EmptyAuthorListException if the authors list is null or empty.
+     */
     public Book(String title, List<Author> authors) throws EmptyAuthorListException {
         this.title = title;
         if (authors == null || authors.isEmpty()) {
@@ -30,10 +48,21 @@ public class Book implements LibraryItem {
         this.authors = authors;
     }
 
+    /**
+     * Returns the list of authors of the book.
+     *
+     * @return A list of Author objects.
+     */
     public List<Author> getAuthors() {
         return authors;
     }
 
+    /**
+     * Sets the list of authors of the book.
+     *
+     * @param authors A list of Author objects to set as the book's authors.
+     * @throws EmptyAuthorListException if the authors list is null or empty.
+     */
     public void setAuthors(List<Author> authors) throws EmptyAuthorListException {
         if (authors == null) {
             throw new EmptyAuthorListException("Author list cannot be null");
@@ -44,6 +73,12 @@ public class Book implements LibraryItem {
         this.authors = authors;
     }
 
+    /**
+     * Adds an author to the book's list of authors.
+     *
+     * @param author The Author object to add to the book.
+     * @throws IllegalArgumentException if the author object is null.
+     */
     public void addAuthor(Author author) {
         if (author == null) {
             throw new IllegalArgumentException("Author cannot be null");
@@ -51,22 +86,46 @@ public class Book implements LibraryItem {
         this.authors.add(author);
     }
 
+    /**
+     * Returns the title of the book.
+     *
+     * @return The book's title.
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Sets the title of the book.
+     *
+     * @param title The title to set for the book.
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Sets the book's cleaning status.
+     *
+     * @param cleaningStatus A boolean representing whether the book is being cleaned.
+     */
     public void setCleaning(boolean cleaningStatus){
         this.cleaning = cleaningStatus;
     }
 
+    /**
+     * Returns the book's cleaning status.
+     *
+     * @param book The book for which to get the cleaning status.
+     * @return A boolean indicating whether the book is in the process of being cleaned.
+     */
     public boolean getCleaning(Book book){
         return cleaning;
     }
     
+    /**
+     * Performs the cleaning operation on the book, setting its cleaning status to true.
+     */
     public void clean() {
         setCleaning(true);
     }
